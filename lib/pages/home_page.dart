fc0bc0240ce448e24c99f3697b2054051a2662ad
samuelematias <hi@samuelematias.com>
2020-07-21 13:32:29 -0300

    feat(lib): add theme_provider lib

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Hello!'),
+      ],
+    );
+  }
+}
